fa2e67f7773a79865a3d8d4210a3a11345114304
Alaa Abdel Latif <al.a.latif.94@gmail.com>
2021-05-08 11:14:24 -0700

    fix issue with metadata file naming

--- src/biolabs_release.py
+++ src/biolabs_release.py
@@ -68,14 +68,14 @@ if __name__=="__main__":
     transfer_fasta_cmd = f"cp {fasta_hub}/* {seqs_dir}/."
     bs.run_command(transfer_fasta_cmd)
     print(f"Transferring metadata from Windows to Linux subsystem")
-    transfer_meta_cmd = f"cp {meta_hub}/* {out_dir}/gisaid_metadata_release.xls"
+    transfer_meta_cmd = f"cp {meta_hub}/* {out_dir}/{date}_gisaid_metadata_release.xls"
     bs.run_command(transfer_meta_cmd)
     accepted_seqs_dir = Path(out_dir/'fa_accepted')
     if not Path.isdir(accepted_seqs_dir):
         Path.mkdir(accepted_seqs_dir);
-    intro = pd.read_excel(f'{out_dir}/{meta_fn}', sheet_name='Instructions')
-    cov = pd.read_excel(f'{out_dir}/{meta_fn}', sheet_name='Coverage')
-    meta = pd.read_excel(f'{out_dir}/{meta_fn}', sheet_name='Submissions', skiprows=1)
+    intro = pd.read_excel(f'{out_dir}/{date}_gisaid_metadata_release.xls', sheet_name='Instructions')
+    cov = pd.read_excel(f'{out_dir}/{date}_gisaid_metadata_release.xls', sheet_name='Coverage')
+    meta = pd.read_excel(f'{out_dir}/{date}_gisaid_metadata_release.xls', sheet_name='Submissions', skiprows=1)
     qc_filter = (cov['pct_coverage']>min_coverage) & (cov['avg_depth']>min_depth)
     accepted_samples = cov.loc[qc_filter, 'Biolab Trans. #'].tolist()
     meta['sample_id'] = meta['FASTA filename'].apply(lambda x : x.split('_')[1].split('.')[0]).astype(int)
